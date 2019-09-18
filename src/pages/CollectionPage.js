b6393c36df59ddb3104d1df0c42daf87d436b21a
tanamet.t <tanamet.tpk@gmail.com>
2019-09-18 20:50:50 +0700

    + Collection page

--- src/pages/CollectionPage.js
+++ src/pages/CollectionPage.js
@@ -1,17 +1,58 @@
 import React , {useState} from 'react'
-import Card from '../components/Card'
+import TitleInput from '../components/Input'
+import {Button} from '../components/Button'
 import Header , {HeaderItem , HeaderZone} from '../components/header'
 import {Link} from 'react-router-dom'
 import CollectionList from '../components/CollectionList'
+import Modal from 'react-modal'
 
 const EmptyCard = (props) => {
 
+    const customStyles = {
+        overlay: {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 1000,
+            backgroundColor: 'rgba(0, 0, 0, 0.75)'
+        },
+        content : {
+          top                   : '50%',
+          left                  : '50%',
+          right                 : 'auto',
+          bottom                : 'auto',
+          marginRight           : '-50%',
+          transform             : 'translate(-50%, -50%)',
+        }
+      };
+
     let [isOpen , setOpen] = useState(false)
+    let [name , setName] = useState("")
+
+    const create = () => {
+        if (props.action) props.action(name)
+        setOpen(false)
+    }
 
     return(
         <div>
 
-            <div className="empty-card">
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={() => setOpen(false)}
+                contentLabel="Create Collection"
+                style={customStyles}
+            >
+                <div style={{display:"grid" , gridGap:"12px"  , gridTemplateRows:"2fr 2fr 1fr"}}>
+                    <h1>Create Collection</h1>
+                    <TitleInput value={name} placeholder="Collection name" onChange={({target:{value}}) => setName(value) } />
+                    <Button primary text="Create" action={() => create()}/>
+                </div>
+            </Modal>
+
+            <div className="empty-card" style={{width:"100%" , height:"100%"}} onClick={() => { setOpen(true) }}>
                 <h3>Create new Collection</h3>
             </div>
 
@@ -25,6 +66,13 @@ const CollectionPage = (props) => {
     const logout = () => {
         console.log("hello");
         
+    }
+
+    const createCollection = (name) => {
+
+        console.log(name);
+        
+
     }
 
     return (
@@ -49,11 +97,7 @@ const CollectionPage = (props) => {
                 <h1>Collections</h1>
                 <div style={{display:"grid" , gridGap:"28px"  , gridTemplateColumns:"1fr 1fr 1fr 1fr"}}>
 
-                    <div className="empty-card">
-                        <h3>Create new Collection</h3>
-                    </div>
-
-                    <EmptyCard />
+                    <EmptyCard action={createCollection} />
 
                     <CollectionList collections={[{id:3 , name:"hello word"} , {id:1 , name:"gg"}]} />
 
--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import Header , {HeaderItem , HeaderZone} from '../components/header'
+import {Link} from 'react-router-dom'
+import Card from '../components/Card'
 
 const NotePage = (props) => {
+
+    const logout = () => {
+        console.log("hello");
+        
+    }
+
     return (
         <div>
-            note page with id = {props.match.params.id}
+            <Header height="80px" backgroundColor="#151b26" isFloat={true} shadow={true} >
+
+                <HeaderZone paddingLeft="50px">
+                    <HeaderItem justifyContent={"start"} >
+                        <h1>Noter</h1>
+                    </HeaderItem>
+                </HeaderZone>
+
+                <HeaderZone position={'right'} size={4} paddingRight="50px">
+                    <HeaderItem justifyContent={"start"} >
+                        <Link to="/collection" onClick={logout} style={{textDecoration:"none" , color:"white" , paddingRight:"30px"}}>collections</Link>
+                    </HeaderItem>
+                    <HeaderItem justifyContent={"start"} >
+                        <Link to="/" onClick={logout} style={{textDecoration:"none" , color:"white"}}>logout</Link>
+                    </HeaderItem>
+                </HeaderZone>
+
+            </Header>
+
+            <div style={{ padding:"8% 10% 0px" , minHeight:"100vh" , backgroundColor:"#f6f8f9"}}>
+                <h1>Notes</h1>
+                <div style={{display:"flex" }}>
+
+                    <Card customStyle={{ width:"100%" , minHeight:"180px" , padding:"0px 24px 12px" }}>
+                        <p style={{fontSize:"24px"}}>Title</p>
+                        <textarea placeholder="Take a note..." />
+                        <div style={{ height:'80px' , display:"flex" , justifyContent:"flex-end" , marginTop:"12px" }} >
+                            <div className="note-button">
+                                Note
+                            </div>
+                        </div>
+                    </Card>
+
+                </div>
+            </div>
         </div>
     )
 }
